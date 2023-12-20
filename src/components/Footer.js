47af39d92bf30f62ce007db778412443f46cac36
mahadev-zervio <mahadev.github@zervio>
2023-12-21 00:27:19 +0530

    this is my first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Survey App. All rights reserved.</p>
+        <p>Privacy Policy | Terms of Service</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
