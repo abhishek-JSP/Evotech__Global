febfd9ee28fbf8a2e96a3d2770bfe688e7d0ce9d
mahadev-zervio <mahadev.github@zervio>
2023-12-21 01:43:52 +0530

    newchange

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ import LogIn from "./components/LogIn";
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<SurveyForm />} />
-          <Route path="/data-viewer" element={<DataViewer />} />
-          <Route path="/login" element={<LogIn />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<SurveyForm />} />
+        <Route path="/data-viewer" element={<DataViewer />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </div>
   );
 };
 
