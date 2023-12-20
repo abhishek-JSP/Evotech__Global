9c4202ac0d69499e7f59c0122a70e99b3336dc37
mahadev-zervio <mahadev.github@zervio>
2023-12-21 01:13:32 +0530

    secound commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import SurveyForm from "./components/SurveyForm";
 import DataViewer from "./components/DataViewer";
 import "./App.css";
 import LogIn from "./components/LogIn";
 
-
 const App = () => {
-  
   return (
     <Router>
       <div className="app">
-          <Routes>
-            <Route path="/" element={<SurveyForm />} />
-            <Route path="/data-viewer" element={<DataViewer />} />
-            <Route path="/login" element={<LogIn />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<SurveyForm />} />
+          <Route path="/data-viewer" element={<DataViewer />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
       </div>
     </Router>
   );
