e9688ad1a578429a72536ef1818f65d549d15ce1
mahadev-zervio <mahadev.github@zervio>
2023-12-21 01:16:19 +0530

    third change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import SurveyForm from "./components/SurveyForm";
 import DataViewer from "./components/DataViewer";
 import "./App.css";
